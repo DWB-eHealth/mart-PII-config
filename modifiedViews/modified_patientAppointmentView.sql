326cdb445df0365824b90384c507db1deb48fb8b
kris-reinhardt <54648029+kris-reinhardt@users.noreply.github.com>
2022-02-06 20:43:04 -0500

    Create modified_patientAppointmentView.sql

---
+++ modifiedViews/modified_patientAppointmentView.sql
@@ -0,0 +1,22 @@
+SELECT
+  pd.person_id AS patient_id,
+  pd.gender,
+  pd.birthyear               AS birth_year,
+  EXTRACT(YEAR FROM (SELECT age( pap.appointment_start_time, TO_DATE(CONCAT('01-01-', pd.birthyear), 'dd-MM-yyyy')))) AS age_at_appointment,
+  age_group(pap.appointment_start_time, TO_DATE(CONCAT('01-01-', pd.birthyear), 'dd-MM-yyyy')) As age_group_at_appointment,
+  pd.dead,
+  pa.*,
+  pap.appointment_id,
+  pap.appointment_start_time,
+  pap.appointment_end_time,
+  pap.appointment_speciality,
+  pap.appointment_service,
+  pap.appointment_service_duration,
+  pap.appointment_service_type,
+  pap.appointment_service_type_duration,
+  pap.appointment_status,
+  pap.appointment_location,
+  pap.appointment_kind
+FROM person_details_default pd
+  LEFT JOIN person_attributes pa ON pa.person_id = pd.person_id
+  LEFT JOIN patient_appointment_default pap ON pap.patient_id = pd.person_id
