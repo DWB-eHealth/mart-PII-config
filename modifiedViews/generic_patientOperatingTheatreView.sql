e9fbf0ed62ffb45a1ef71a7c99cebf6b432d2434
kris-reinhardt <54648029+kris-reinhardt@users.noreply.github.com>
2022-02-06 20:47:45 -0500

    Create generic_patientOperatingTheatreView.sql

---
+++ modifiedViews/generic_patientOperatingTheatreView.sql
@@ -0,0 +1,31 @@
+SELECT 
+	pd.person_id AS patient_id,
+	pd.gender,
+	pd.birthyear AS birth_year,
+	EXTRACT(YEAR FROM (SELECT age(sb.block_starttime, TO_DATE(CONCAT ('01-01-',pd.birthyear), 'dd-MM-yyyy')))) AS age_at_surgery,
+	age_group(sb.block_starttime, TO_DATE(CONCAT ('01-01-',pd.birthyear), 'dd-MM-yyyy')) AS age_group_at_surgery,
+	pd.dead,
+	pa.*,
+	sa.surgical_block_id,
+	sb.primary_provider_name,
+	sb.location_name,
+	sb.creator_name AS block_creator_name,
+	sb.date_created AS block_date_created,
+	sb.date_changed AS block_date_changed,
+	sb.changed_by AS block_changed_by,
+	sb.block_starttime AS block_start_time,
+	sb.block_endtime AS block_end_time,
+	saa.*,
+	sa.sort_weight AS surgery_sort_weight,
+	sa.STATUS AS surgery_status,
+	sa.actual_start_datetime AS surgery_actual_start_time,
+	sa.actual_end_datetime AS surgery_actual_end_time,
+	sa.date_created AS surgery_date_created,
+	sa.date_changed AS surgery_date_changed,
+	sa.creator_name AS surgery_creator_name,
+	sa.changed_by AS surgery_date_changed_by,
+FROM person_details_default pd
+LEFT JOIN person_attributes pa ON pa.person_id = pd.person_id
+LEFT JOIN surgical_appointment_default sa ON sa.patient_id = pd.person_id
+LEFT JOIN surgical_block_default sb ON sb.surgical_block_id = sa.surgical_block_id
+LEFT JOIN surgical_appointment_attributes saa ON saa.surgical_appointment_id = sa.surgical_appointment_id
